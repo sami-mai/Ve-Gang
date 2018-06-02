056605db9b8d22bfca31b819663921af3c94772b
sami-mai <samirah.maison@gmail>
2018-06-02 08:50:05 +0300

    restructure project

--- thirdauth/views.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
-@login_required
-def account(request):
-
-    return render(request, 'thirdauth/account.html', context=None)
